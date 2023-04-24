94d4f1c1e87d8bf8e3f0d342ed78138fadec62c5
vossie <carel.vosloo@bronzegate.com>
2023-04-24 12:44:04 +0100

    Upgraded to v1.0.15-1

--- src/main/scala/com/ziqni/transformer/SampleSQSWithCallbacks.scala
+++ src/main/scala/com/ziqni/transformer/SampleSQSWithCallbacks.scala
@@ -1,19 +1,16 @@
 package com.ziqni.transformer
 
+import com.ziqni.transformers.defaults.Event
 import com.ziqni.transformers.domain._
 import com.ziqni.transformers.webhooks.{CustomWebhookSettings, CustomWebhooks}
 import com.ziqni.transformers.{ZiqniContext, ZiqniMqTransformer}
 import org.joda.time.DateTime
-import org.json4s.DefaultFormats
-import org.json4s.jackson.parseJson
 
 import scala.language.implicitConversions
 
 class SampleSQSWithCallbacks extends ZiqniMqTransformer with CustomWebhooks with CustomFieldEntryImplicits {
-  private implicit val formats: DefaultFormats.type = DefaultFormats
 
   private val PostToUrl: String = "<<some-url>>"
-
 
   implicit def toBooleanCustomFieldEntry(v: Boolean): CustomFieldEntryText = CustomFieldEntryText(v.toString)
 
@@ -22,17 +19,11 @@ class SampleSQSWithCallbacks extends ZiqniMqTransformer with CustomWebhooks with
    */
   override def apply(message: Array[Byte], ziqniContext: ZiqniContext, args: Map[String, Any]): Unit = {
     implicit val context: ZiqniContext = ziqniContext
-    val messageAsString = ZiqniContext.convertByteArrayToString(message)
-
-    parseJson(messageAsString)
-      .extract[List[DefaultEvent]]
-      .foreach(a1 =>
-        handleEvent(a1)
-      )
+    handleEvent(Event.fromByteArray(message))
   }
 
-  private def handleEvent(reevoEventMessage: DefaultEvent)(implicit ziqniContext: ZiqniContext): Unit = {
-    ziqniContext.ziqniApiAsync.pushEventTransaction(reevoEventMessage.asBasicEventModel)
+  private def handleEvent(event: Event)(implicit ziqniContext: ZiqniContext): Unit = {
+    ziqniContext.ziqniApiAsync.pushEventTransaction(event.asBasicEventModel)
   }
 
   ////////////////////////////////////////////////////////////
@@ -120,94 +111,4 @@ class SampleSQSWithCallbacks extends ZiqniMqTransformer with CustomWebhooks with
   override def onAchievementRewardIssued()(implicit settings: CustomWebhookSettings, basicEntityChanged: BasicEntityChanged, timestamp: DateTime, additionalFields: Map[String, Any], ziqniContext: ZiqniContext): Unit = super.onAchievementRewardIssued()
 
   override def onAchievementRewardClaimed()(implicit settings: CustomWebhookSettings, basicEntityStateChanged: BasicEntityStateChanged, timestamp: DateTime, additionalFields: Map[String, Any], ziqniContext: ZiqniContext): Unit = super.onAchievementRewardClaimed()
-
-  private case class DefaultEvent(
-                                   memberRefId: String,
-                                   action: String,
-                                   batchId: Option[String],
-                                   entityRefId: String,
-                                   sourceValue: Double,
-                                   transactionTimestamp: DateTime,
-                                   tags: scala.Seq[String],
-                                   eventRefId: String,
-                                   memberId: Option[String],
-                                   customFields: Map[String, Any]
-                                 ) extends CustomFieldEntryImplicits {
-
-    def fromByteArray(message: Array[Byte]): DefaultEvent = {
-      implicit val formats: DefaultFormats.type = DefaultFormats
-      val messageAsString = ZiqniContext.convertByteArrayToString(message)
-      parseJson(messageAsString).extract[DefaultEvent]
-    }
-
-    def asBasicEventModel: BasicEventModel = {
-
-      def handleSeq(key: String, list: Seq[_]): (String, CustomFieldEntry[_ <: Any]) = {
-        list.headOption.map {
-          case _: String =>
-            (key, toStringList(list))
-          case _: Boolean =>
-            (key, toStringList(list))
-
-          case _: Int =>
-            (key, toNumList(list))
-          case _: Double =>
-            (key, toNumList(list))
-          case _: Long =>
-            (key, toNumList(list))
-          case _ =>
-            (key, CustomFieldEntryText(""))
-
-        }.getOrElse((key, toNumList(list)))
-      }
-
-      def toStringList(s: Seq[_ <: Any]): CustomFieldEntry[_ <: Any] = {
-        CustomFieldEntryTextArray(s.map {
-          case s: String => s
-          case b: Boolean => b.toString.toLowerCase
-          case s: Any => s.toString
-        })
-      }
-
-      def toNumList(s: Seq[_ <: Any]): CustomFieldEntry[_ <: Any] = {
-        CustomFieldEntryNumberArray(s.map {
-          case in: Int => in.toDouble
-          case in: Double => in
-          case in: Long => in.toDouble
-        })
-      }
-
-      val cf: Map[String, CustomFieldEntry[_ <: Any]] = this.customFields.map(customFields => customFields._2 match {
-        case in: String =>
-          (customFields._1, in)
-        case in: Int =>
-          (customFields._1, in)
-        case in: Double =>
-          (customFields._1, in)
-        case in: Long =>
-          (customFields._1, in)
-        case in: Boolean =>
-          (customFields._1, in)
-        case list: List[_] =>
-          handleSeq(customFields._1, list)
-        case list: Seq[_] =>
-          handleSeq(customFields._1, list)
-        case _ =>
-          (customFields._1, CustomFieldEntryText(""))
-      })
-
-      BasicEventModel(
-        memberId = memberId,
-        memberRefId = memberRefId,
-        entityRefId = entityRefId,
-        eventRefId = entityRefId,
-        batchId = batchId,
-        action = action,
-        sourceValue = sourceValue,
-        transactionTimestamp = transactionTimestamp,
-        tags = tags,
-        customFields = cf
-      )
-    }
-  }
 }
